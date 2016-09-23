2309526438ea81c81db1a2584b282692c3430f10
Christopher Van <cvan@mozilla.com>
2016-09-22 22:18:39 -0700

    do not open external links in new tabs

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,6 @@
 (function () {
   document.addEventListener('DOMContentLoaded', function () {
-    // Open external links in new tabs.
-    var links = document.querySelectorAll('a[href^="//"], a[href^="http://"], a[href^="https://"]');
-    for (var i = 0, len = links.length; i < len; ++i) {
-      links[i].setAttribute('target', '_blank');
-    }
-
-    // Strip `.html` from ugly URLs (when served on GitHub Pages).
+    // Strip `.html` from ugly URLs (when served in production).
     if (location.hostname === 'iswebvrready.org') {
       var relativeLinks = document.querySelectorAll('a[href$=".html"]:not([href^="//"]):not([href^="http://"]):not([href^="https://"])');
       for (i = 0, len = relativeLinks.length; i < len; ++i) {
